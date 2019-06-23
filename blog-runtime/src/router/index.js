61b5c6dd9fce92c08ff2a001e98c90ad73c9e8e5
gaarahan <815199630@qq.com>
2019-06-23 14:59:32 +0800

    purpose : adjust the project structure, add server and the hexo test dir person : gaarahan-arch

--- blog-runtime/src/router/index.js
+++ blog-runtime/src/router/index.js
@@ -4,7 +4,6 @@ import Router from 'vue-router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-import app from '../App'
 import viewBlog from '../components/viewBlog'
 import addBlog from '../components/addBlog'
 
@@ -13,7 +12,7 @@ Vue.use(VueAxios, axios)
 
 export default new Router({
   routes: [
-    { path: '/', component: app },
+    { path: '/', redirect: '/view' },
     { path: '/view', component: viewBlog },
     { path: '/add', component: addBlog }
   ]
